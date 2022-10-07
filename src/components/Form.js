219a5346a2d8167ccb359f93d8332d1114946d9f
Sheeraz-Kalhoro <sheeraz@outlook.com>
2022-10-07 18:06:59 +0500

    code re-edited

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,40 +10,38 @@ import DispatchContext from "../DispatchContext";
 const Form = () => {
   const { state } = useContext(OurContext);
   const dispatch = useContext(DispatchContext);
-  // const [discount, setDiscount] = useState(false);
+  const [discount, setDiscount] = useState(5);
   // console.log(state.singleSpace, state.doubleSpace);
-  const page = state.page
+  const page = state.page;
   // const words = state.singleSpace? state.words
   // const words = state.singleSpace? state.words = 550: state.words = 275
   let totalPrice =
-  state.tab.tabPrice +
-  state.gigWork.gigTypePrice +
-  state.almaMater.levelPrice +
-  state.time.durationPrice
-  
-  totalPrice =state.singleSpace?totalPrice*2:totalPrice
-  
+    state.tab.tabPrice +
+    state.gigWork.gigTypePrice +
+    state.almaMater.levelPrice +
+    state.time.durationPrice;
+
+  totalPrice = state.singleSpace ? totalPrice * 2 : totalPrice;
+
   // console.log(state.gigWork)
   function submitHandler(e) {
     e.preventDefault();
   }
-  function wordsdispatcher(words){
-    dispatch({type:"SINGLE-SPACE",value:words})
+  function wordsdispatcher(words) {
+    dispatch({ type: "SINGLE-SPACE", value: words });
   }
-  function dispatchtotal(totalPrice, page, ){
-    dispatch({type:'TOTAL', value:totalPrice * page})
-  
+  function dispatchtotal(totalPrice, page) {
+    dispatch({ type: "TOTAL", value: totalPrice * page });
   }
   useEffect(() => {
-    dispatchtotal(totalPrice,page)
-  }, [totalPrice,page])
+    dispatchtotal(totalPrice, page);
+  }, [totalPrice, page]);
   // useEffect(() => {
-  //   if((state.singleSpace === true )|| (state.page>=2 && state.words >= 550))
-  //     setDiscount(true)
-  //   else
-  //     setDiscount(false)
-  // }, [state.singleSpace, state.page])
-  
+  //   if (state.singleSpace === true || (state.page >= 2 && state.words >= 550))
+  //     setDiscount(true);
+  //   else setDiscount(false);
+  // }, [state.singleSpace, state.page]);
+
   console.log(state.page, state.words);
   return (
     <div className="form rounded-2 p-3">
@@ -64,7 +62,7 @@ const Form = () => {
             id="flexRadioDefault1"
             value={state.doubleSpace}
             defaultChecked={state.doubleSpace}
-            onChange={() => dispatch({ type: "DOUBLE-SPACE", value:true})}
+            onChange={() => dispatch({ type: "DOUBLE-SPACE", value: true })}
           />
           <label className="form-check-label" htmlFor="flexRadioDefault1">
             Double spaces
@@ -75,21 +73,23 @@ const Form = () => {
             name="singleSpace"
             value={state.singleSpace}
             id="flexRadioDefault1"
-            onChange={() => dispatch({ type: "SINGLE-SPACE" ,value:true})}
+            onChange={() => dispatch({ type: "SINGLE-SPACE", value: true })}
           />
           <label className="form-check-label" htmlFor="flexRadioDefault1">
             Single space
           </label>
         </div>
-        {/* {discount && <div className="">
-              <h5 className="m-0 fw-bold">$ {discount} OFF</h5>
-              {console.log(discount)}
-            </div>} */}
-        <div className="d-flex justify-content-end align-items-center p-3">
-          <h5 className="m-0 fw-bold">$ {state.totalPrice}</h5>
-          {/* <h5 className="m-0 fw-bold">$ {state.page * totalPrice}</h5> */}
-          {console.log(state.totalPrice)}
-          <img src={fire} width="24px" height="24px" alt="burnImage" />
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="discount d-flex justify-content-between align-items-center p-3">
+            <p className="discount-p  m-0 text-white text-sm-start rounded-1 p-1">$ {discount} OFF </p><span className="line-through p-1">({state.totalPrice.toFixed(2)})</span>
+            {/* {console.log(discount)} */}
+          </div>
+          <div className="d-flex justify-content-end align-items-center p-3">
+            <h5 className="m-0 fw-bold">$ {state.totalPrice.toFixed(2)}</h5>
+            {/* <h5 className="m-0 fw-bold">$ {state.page * totalPrice}</h5> */}
+            {console.log(state.totalPrice)}
+            <img src={fire} width="24px" height="24px" alt="burnImage" />
+          </div>
         </div>
         <div className="d-grid gap-2">
           <button className="btn btn-primary" type="button">
